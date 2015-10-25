86bb7706183aef842424804ba06108c780443c9e
Theodore Lytras <thlytras@gmail.com>
2015-10-25 13:12:42 +0200

    Rewritten to provide up to four curves in one plot, plus boxplots.

--- server.R
+++ server.R
@@ -1,26 +1,21 @@
 
-# This is the server logic for a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
 
-naiplot <- function(x, x2=NULL, col="blue", col2="red", lwd=1, xlim=NA, xlab=NA, ylab="Density", legend=NA, legendText=NA, main=NULL, axis=NA, axislabels=NA, barplot=FALSE) {
-  two <- !is.null(x2) # Do we have a double plot?
-  d <- density(x) # Get density for the first sample
+naiplot <- function(x, col=c("blue", "red", "green", "magenta"), lwd=1, 
+                    xlim=NA, xlab=NA, ylab="Density", legend=NA, 
+                    legendText=NA, main=NULL, axis=NA, axislabels=NA, 
+                    cex=1, boxplot=FALSE, RI=NA) {
+  if (!is.list(x)) x <- list(x)
+  n <- length(x)
+  col <- rep_len(col, n)
+  
+  d <- lapply(x, density)
+  
+  two <- (n>1)
   
   # Get y- and x- value ranges
-  ry <- range(d$y); rx <- range(d$x)
-  
-  if (two) {
-    d2 <- density(x2) # Get density estimate for the second sample
-    
-    # Adjust y- and x-value ranges, so that the plot fits both curves
-    if (max(d2$y)>max(d$y)) ry <- range(d2$y)
-    rx <- range(c(rx, range(d2$x)))
-  }
+  ry <- range(sapply(d, function(di) range(di$y)))
+  rx <- range(sapply(d, function(di) range(di$x)))
   
   # Calculate plot xlim and ylim
   if (is.na(xlim)) {
@@ -29,24 +24,51 @@ naiplot <- function(x, x2=NULL, col="blue", col2="red", lwd=1, xlim=NA, xlab=NA,
     rx <- xlim
   }
   ry0 <- pretty(ry); ry0 <- ry0[ry0>=0]
-  ry <- range(ry) + c(-diff(range(ry))*(0.20 + 0.10*two), diff(range(ry))*(0.10+0.10*barplot*(two+1)))
+  ry <- range(ry) + c(-diff(range(ry))*(0.20 + 0.10*(n-1)), diff(range(ry))*(0.10+0.10*boxplot*n))
+  
+  rug_pos <- ry[1]/n * (1:n - 1)
+  rug_ticksize <- 0.105*(2/3)^(n-1)
   
   # Plotting the first sample
-  plot(d, ylim=ry, yaxs="i", xlim=rx, yaxs="i", bty="l", 
-       col=col, lwd=lwd, xlab=xlab, ylab=ylab, main=main, 
-       xaxt="n", yaxt="n")
-  axis(2, at=ry0, labels=ry0)
-  rug(x, ticksize=0.07*((1-two)*0.5+1), col=col, lwd=lwd/2)
+  par(mar=c(5,5,4,2))
+  plot(d[[1]], ylim=ry, yaxs="i", xlim=rx, yaxs="i", bty="l", 
+       col=col[1], lwd=lwd, xlab=xlab, ylab=ylab, main=main, 
+       xaxt="n", yaxt="n", cex.lab=cex, cex.main=cex)
+  axis(2, at=ry0, labels=ry0, cex.axis=cex)
+  rug(x[[1]], ticksize=rug_ticksize, col=col[1], lwd=max(1,lwd/2))
   
-  if (two) { # Plot the second sample
-    points(d2, type="l", col=col2, lwd=lwd)
-    rug(x2, pos=ry[1]/2, ticksize=0.07, col=col2, lwd=lwd/2)
+  if (n>1) {
+    for (i in 2:n) {
+      points(d[[i]], type="l", col=col[i], lwd=lwd)
+      rug(x[[i]], pos=rug_pos[i], ticksize=rug_ticksize, col=col[i], lwd=max(1,lwd/2))
+    }
   }
   
-  if (length(axis)==1 && is.na(axis)) axis(1) else axis(1, at=axis, label=axislabels)
+  if (length(axis)==1 && is.na(axis)) axis(1, cex.axis=cex) else axis(1, at=axis, label=axislabels, cex.axis=cex)
   
   if (!is.na(legend)) {
-    legend(legend, col=c(col, col2)[1:(two+1)], lwd=rep(lwd,two+1), legend=legendText[1:(two+1)], bty="n", inset=0.005, y.intersp=1.5)
+    legend(legend, col=col, lwd=rep(lwd,n), legend=legendText[1:n], 
+           bty="n", inset=0.005, y.intersp=1.5, cex=cex)
+  }
+  
+  if (!is.na(RI)) {
+    if (RI=="a") {
+      abline(v=log10(c(10,100)), lty=c("dotted", "dashed"), lwd=2*lwd, col="darkred")
+    }
+    if (RI=="b") {
+      abline(v=log10(c(5,50)), lty=c("dotted", "dashed"), lwd=2*lwd, col="darkred")
+    }
+  }
+  
+  if (boxplot) {
+    # 0.10*boxplot*(two+1)
+    ry0 <- ry[2]/(1.1+0.10*boxplot*n)
+    
+    boxplot_pos <- ry0*1.03 + (ry[2]-ry0)/(n+1) * (1:n)
+    for (i in 1:n) {
+      boxplot(x[[i]], border=col[i], horizontal=TRUE, add=TRUE, lwd=max(1,lwd/2), outline=FALSE, axes=FALSE,
+              boxwex=2/30 + n/30, at=boxplot_pos[i])
+    }
   }
   
 }
@@ -59,19 +81,36 @@ shinyServer(function(input, output) {
     suppressWarnings({
       values1 <- as.numeric(unlist(strsplit(input$numvalues1, "\\s*(,|\\s)\\s*")))
       values2 <- as.numeric(unlist(strsplit(input$numvalues2, "\\s*(,|\\s)\\s*")))
+      values3 <- as.numeric(unlist(strsplit(input$numvalues3, "\\s*(,|\\s)\\s*")))
+      values4 <- as.numeric(unlist(strsplit(input$numvalues4, "\\s*(,|\\s)\\s*")))
       axisval <- as.numeric(unlist(strsplit(input$axisval, "\\s*(,|\\s)\\s*")))
       axislab <- trimws(unlist(strsplit(input$axislab, "\\s*(,|\\s)\\s*")))
     })
+    if (input$logme) {
+      values1 <- log10(values1); values2 <- log10(values2)
+      values3 <- log10(values3); values4 <- log10(values4)
+    }
+    if (sum(is.na(values2)) == length(values2)) values2 <- NULL
+    if (sum(is.na(values3)) == length(values3)) values3 <- NULL
+    if (sum(is.na(values4)) == length(values4)) values4 <- NULL
+    n <- 1
+    if (input$secondPlot && !is.null(values2)) n <-2
+    if (n==2 && input$thirdPlot && !is.null(values3)) n <- 3
+    if (n==3 && input$fourthPlot && !is.null(values4)) n <- 4
+    
     if (input$legend=="0") legend <- NA else legend <- input$legend
-    if (!input$secondPlot || sum(is.na(values2)) == length(values2)) values2 <- NULL
     if (!input$custAxis || sum(is.na(axisval)) == length(axisval)) axisval <- NA
-
+    RI <- ifelse(input$addRI, input$RItype, NA)
+    
     # draw the histogram with the specified number of bins
     if (length(values1[!is.na(values1)])>0) {
-      naiplot(values1, values2, legend=legend, lwd=input$lwd, 
-              legendText = c(input$name1, input$name2)[1:(input$secondPlot+1)],
-              col=input$col1, col2=input$col2, main=input$main,
-              axis=axisval)
+      naiplot(list(values1, values2, values3, values4)[1:n],
+              col = c(input$col1, input$col2, input$col3, input$col4)[1:n],
+              legend=legend, lwd=input$lwd, 
+              legendText = c(input$name1, input$name2, input$name3, input$name4)[1:n],
+              main=input$main, xlab=input$xlab,
+              axis=axisval, axislabels=axislab[1:length(axisval)], cex=input$cex,
+              RI=RI, boxplot=input$boxplot)
     }
 
   })
--- ui.R
+++ ui.R
@@ -1,11 +1,6 @@
 
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
-test <- log(c(0.04, 0.09, 0.19, 0.22, 0.22, 0.24, 0.24, 0.24, 0.26, 0.32, 0.32, 0.32, 0.34, 0.37, 0.39, 0.41, 0.47, 0.49, 0.52, 0.54, 0.56, 0.58, 0.58, 0.60, 0.65, 0.67, 0.67, 0.67, 0.69, 0.73, 0.75, 0.75, 0.77, 0.80, 0.82, 0.84, 0.86, 0.86, 0.88, 0.90, 0.90, 0.97, 0.97, 1.03, 1.03, 1.08, 1.12, 1.12, 1.14, 1.18, 1.18, 1.18, 1.25, 1.31, 1.31, 1.35, 1.38, 1.51, 1.51, 1.55, 1.57, 1.57, 1.59, 1.59, 1.61, 1.61, 1.66, 1.76, 1.81, 1.89, 1.94, 1.94, 1.98, 2.00, 2.37, 2.62, 3.12, 3.59, 4.19, 4.26, 4.32, 340.71, 502.69, 604.09, 732.80, 1082.19))
+test1 <- c(0.04, 0.09, 0.19, 0.22, 0.22, 0.24, 0.24, 0.24, 0.26, 0.32, 0.32, 0.32, 0.34, 0.37, 0.39, 0.41, 0.47, 0.49, 0.52, 0.54, 0.56, 0.58, 0.58, 0.60, 0.65, 0.67, 0.67, 0.67, 0.69, 0.73, 0.75, 0.75, 0.77, 0.80, 0.82, 0.84, 0.86, 0.86, 0.88, 0.90, 0.90, 0.97, 0.97, 1.03, 1.03, 1.08, 1.12, 1.12, 1.14, 1.18, 1.18, 1.18, 1.25, 1.31, 1.31, 1.35, 1.38, 1.51, 1.51, 1.55, 1.57, 1.57, 1.59, 1.59, 1.61, 1.61, 1.66, 1.76, 1.81, 1.89, 1.94, 1.94, 1.98, 2.00, 2.37, 2.62, 3.12, 3.59, 4.19, 4.26, 4.32, 340.71, 502.69, 604.09, 732.80, 1082.19)
+test2 <- c(0.21, 0.25, 0.31, 0.33, 0.40, 0.40, 0.42, 0.42, 0.44, 0.52, 0.62, 0.62, 0.67, 0.71, 0.73, 0.75, 0.77, 0.81, 0.92, 0.92, 0.96, 1.00, 0.52, 0.54, 0.58, 0.60, 0.60, 0.60, 0.62, 0.65, 0.67, 0.67, 0.69, 0.71, 0.73, 0.75, 0.77, 0.87, 0.77, 0.92, 0.92, 0.96, 1.00, 1.00, 1.02, 1.06, 1.06, 1.06, 1.06, 1.12, 1.17, 1.17, 1.21, 1.23, 1.23, 1.29, 1.33, 1.38, 1.38, 1.40, 1.42, 1.44, 1.48, 1.50, 1.50, 1.54, 1.56, 1.58, 1.62, 1.69, 1.73, 1.73, 1.77, 1.79, 1.83, 1.90, 1.94, 1.96, 1.98, 2.08, 2.12, 2.12, 2.31, 2.50, 2.52, 2.77)
 
 library(shiny)
 
@@ -13,36 +8,89 @@ shinyUI(fluidPage(
 
   # Application title
   titlePanel("Neuraminidase Inhibitor plot (NAIplot)"),
-
+  
+  tabsetPanel(
+    tabPanel("Plot", 
+      plotOutput("distPlot")
+    ),
+    tabPanel("Statistics",
+      h2("This is the statistics panel"),
+      p("Statistics of bimodality (Hartigan's dip statistic
+        and bimodality coefficient) will appear here"),
+      br(), br(), br(), br(), br()
+    ),
+    tabPanel("Get help",
+      h2("This is a help page"),
+      p("This will be the help page content"),
+      br(), br(), br(), br(), br()
+    )
+  ),
+  
   # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
-      textInput("main", "Plot title"),
-      helpText("Place your values here"),
-      tags$textarea(id="numvalues1", rows=3, paste(test, collapse=" "), style="width:100%; resize: none;"),
-      textInput("col1", "Color", "blue"),
-      textInput("name1", "Legend text"),
-      checkboxInput("secondPlot", "Add second set of values"),
-      conditionalPanel(
-        "input.secondPlot == true",
-        helpText("Place your values here"),
-        tags$textarea(id="numvalues2", rows=3, paste(test, collapse=" "), style="width:100%; resize: none;"),
-        textInput("col2", "Color", "red"),
-        textInput("name2", "Legend text")
-      ),      
-      
-      br(),
+  fluidRow(
+    column(4,
+      textInput("main", "Plot title:", "H1N1 strains"),
+      textInput("xlab", "X-axis label:", "Fold change (log scale)"),
       selectInput("legend", "Legend:",
                   c("No legend"=0,
                     "Top Left" = "topleft",
                     "Top" = "top",
                     "Top Right" = "topright",
-                    "Right" = "right",
-                    "Bottom Right" = "bottomright",
-                    "Bottom" = "bottom",
-                    "Bottom Left" = "bottomleft",
-                    "Left" = "left")),
-      sliderInput("lwd", "Line width", 1, 4, 1),
+                    "Left" = "left",
+                    "Right" = "right"
+                    )),
+      sliderInput("lwd", "Line width", 1, 4, 1, 1),
+      sliderInput("cex", "Label size", 1, 2, 1.2, 0.1)
+    ),
+    column(4,
+     textInput("col1", "Color", "blue"),
+     conditionalPanel("input.legend != '0'",
+       textInput("name1", "Legend text", "Oseltamivir")
+     ),
+     helpText("Place your values here"),
+     tags$textarea(id="numvalues1", rows=3, paste(test1, collapse=" "), style="width:100%; resize: none;"),
+     conditionalPanel(
+       "input.secondPlot == true",
+       hr(style="height: 12px; border: 0; box-shadow: inset 0 12px 12px -12px rgba(0, 0, 0, 0.5);"),
+       textInput("col2", "Color", "red"),
+       conditionalPanel("input.legend != '0'",
+         textInput("name2", "Legend text", "Zanamivir")
+       ),
+       helpText("Place your values here"),
+       tags$textarea(id="numvalues2", rows=3, paste(test2, collapse=" "), style="width:100%; resize: none;")
+     ),
+     conditionalPanel(
+       "input.thirdPlot == true",
+       hr(style="height: 12px; border: 0; box-shadow: inset 0 12px 12px -12px rgba(0, 0, 0, 0.5);"),
+       textInput("col3", "Color", "green"),
+       conditionalPanel("input.legend != '0'",
+                        textInput("name3", "Legend text")
+       ),
+       helpText("Place your values here"),
+       tags$textarea(id="numvalues3", rows=3, style="width:100%; resize: none;")
+     ),
+     conditionalPanel(
+       "input.fourthPlot == true",
+       hr(style="height: 12px; border: 0; box-shadow: inset 0 12px 12px -12px rgba(0, 0, 0, 0.5);"),
+       textInput("col4", "Color", "magenta"),
+       conditionalPanel("input.legend != '0'",
+                        textInput("name4", "Legend text")
+       ),
+       helpText("Place your values here"),
+       tags$textarea(id="numvalues4", rows=3, style="width:100%; resize: none;")
+     )
+    ),
+    column(4,
+      checkboxInput("logme", "Convert values to log", TRUE),
+      checkboxInput("secondPlot", "Add second set of values"),
+      conditionalPanel(
+        "input.secondPlot == true",
+        checkboxInput("thirdPlot", "Add third set of values")
+      ),
+      conditionalPanel(
+        "input.thirdPlot == true",
+        checkboxInput("fourthPlot", "Add fourth set of values")
+      ),
       checkboxInput("custAxis", "Custom axis values"),
       conditionalPanel(
         "input.custAxis == true",
@@ -50,17 +98,15 @@ shinyUI(fluidPage(
         tags$textarea(id="axisval", rows=3, "", style="width:100%; resize: none;"),
         helpText("Custom axis labels"),
         tags$textarea(id="axislab", rows=3, "", style="width:100%; resize: none;")
+      ),
+      checkboxInput("boxplot", "Add horizontal boxplot(s)"),
+      checkboxInput("addRI", "Add inhibition thresholds"),
+      conditionalPanel(
+        "input.addRI == true",
+        radioButtons("RItype", NULL, c("Type A (10/100)"="a", "Type B (5/50)"="b"))
       )
-      
-      
-      
-    ,style="overflow-y:scroll; max-height:550px"),
-    
-
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("distPlot")
     )
+    
+  #,style="top:75%; position:absolute; overflow-y:scroll; width:100%; height:20%"
   )
 ))
